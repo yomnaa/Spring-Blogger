d99b48683b11b3978fcfe94b52ee454433431ca9
yomnaa <yomnaismail95@gmail.com>
2020-05-06 16:02:42 +0200

    Initial project structure

---
+++ src/test/java/com/selfprojects/SpringBlogger/SpringBloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfprojects.SpringBlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
